bbadcfbb043a0316c908c50c4a4acb19568b072a
Dmitry Terehin <dimterex@mail.ru>
2022-10-10 00:16:41 +0700

    Update version information.

--- app/src/main/java/net/dimterex/sync_client/data/entries/ConnectionsLocalModel.kt
+++ app/src/main/java/net/dimterex/sync_client/data/entries/ConnectionsLocalModel.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "connection_settings")
 data class ConnectionsLocalModel(
-    @PrimaryKey @ColumnInfo(name = "id") val id: Long,
+    @PrimaryKey @ColumnInfo(name = "id") val id: Int,
     @ColumnInfo(name = "ip_address") var ip_address: String,
     @ColumnInfo(name = "ip_port") var ip_port: Int,
     @ColumnInfo(name = "login") var login: String,
